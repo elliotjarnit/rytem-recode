8fbdb208ed4ae769975bffedf854ace75b8bc69b
swiftzerr <swiftzermc@gmail.com>
2022-01-24 11:42:45 -0500

    Make queue class with init and skip function

--- queues.py
+++ queues.py
@@ -1,1 +1,13 @@
 # Queues go here
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        self.now_playing = None
+
+    def skip(self):
+        if len(self.queue) > 0:
+            self.now_playing = self.queue[0]
+            self.queue.pop(0)
+        else:
+            self.now_playing = None
