888a566e14e424535b54d22ec86965e0b17f5748
swiftzerr <swiftzermc@gmail.com>
2022-01-24 10:04:22 -0500

    Add first slash command and on ready

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
 import discord
 
 bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print("Rytem is online!\n\nBot Name: " + bot.user.name + "\nBot ID: " + str(bot.user.id) + "\n\nIn servers:")
+    for guild in bot.guilds:
+        print(str(guild.name))
+
+@bot.slash_command()
+async def play(ctx):
+
+    # Variables
+    author = ctx.author
+    bot_vc = ctx.voice_client.channel
+
