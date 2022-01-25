155ce0212cccc281337034ae940c10d9007e95ee
swiftzerr <swiftzermc@gmail.com>
2022-01-25 12:01:34 -0500

    Made individual top commands and added descriptions to all commands.

--- bot.py
+++ bot.py
@@ -34,8 +34,8 @@ class Music(commands.Cog):
     def __init__(self, dis_bot):
         self.bot = dis_bot
 
-    @commands.slash_command()
-    async def play(self, ctx: commands.Context, *, search):
+    @commands.slash_command(description="Plays a song")
+    async def play(self, ctx: commands.Context, *, song):
 
         if ctx.author.voice is None:
             await send_message(ctx, emojis.no + " You must be in a VC to use this command")
@@ -47,19 +47,19 @@ class Music(commands.Cog):
             if cur_channel_mem_count == 1:
                 await user_channel.connect()
             else:
-                if queue.now_playing == None:
+                if queue.now_playing is None:
                     await user_channel.connect()
                 else:
                     send_message(ctx, emojis.no + " Someone else is using the bot right now")
 
         async with ctx.typing():
-            source = await songdown.Song.create_source(ctx, search, loop=bot.loop)
+            source = await songdown.Song.create_data(ctx, song, loop=self.bot.loop)
             if not source:
-                return send_message(ctx, emojis.no + " Can't find any results for: `" + search + "`")
+                return send_message(ctx, emojis.no + " Can't find any results for: `" + song + "`")
             now_playing = queue.now_playing
             queue.add(source)
 
-    @commands.slash_command()
+    @commands.slash_command(description="Toggles song looping")
     async def loop(self, ctx: commands.Context):
         queue.loop = not queue.loop
         if queue.loop:
@@ -67,20 +67,28 @@ class Music(commands.Cog):
         else:
             await send_message(ctx, emojis.loop + " Disabled")
 
-    @commands.slash_command()
+    @commands.slash_command(description="Votes to skip the song")
     async def skip(self, ctx: commands.Context):
         pass
 
-    @commands.slash_command()
+    @commands.slash_command(description="Makes the bot clear queue and leave the VC (DJ role required)")
     async def leave(self, ctx: commands.Context):
         pass
 
-    @commands.slash_command()
+    @commands.slash_command(description="Forces the current song to skip (DJ role required)")
     async def forceskip(self, ctx: commands.Context):
         pass
 
-    @commands.slash_command()
-    async def top(self, ctx: commands.Context):
+    @commands.slash_command(description="Displays top listeners for specified artist (DJ role required)")
+    async def topartist(self, ctx: commands.Context, *, artist):
+        pass
+
+    @commands.slash_command(description="Displays top listeners for specified song (DJ role required)")
+    async def topsong(self, ctx: commands.Context, *, song):
+        pass
+
+    @commands.slash_command(description="Displays top listeners for specified album (DJ role required)")
+    async def topalbum(self, ctx: commands.Context, *, album):
         pass
 
 
